606c2e5d9a50a20eefbaafbca701dcad5c14ade3
Vuong Xuan Hong <vuongxuanhong@gmail.com>
2018-04-18 11:47:58 +0700

    change name

--- app/src/main/java/ishopgo/com/exhibition/ui/base/BaseApiViewModel.kt
+++ app/src/main/java/ishopgo/com/exhibition/ui/base/BaseApiViewModel.kt
@@ -3,10 +3,6 @@ package ishopgo.com.exhibition.ui.base
 import ishopgo.com.exhibition.domain.ApiService
 import javax.inject.Inject
 
-/**
- * @author Steve
- * @since 10/22/17
- */
 open class BaseApiViewModel : BaseViewModel() {
     companion object {
         private val TAG = "BaseAppViewModel"
