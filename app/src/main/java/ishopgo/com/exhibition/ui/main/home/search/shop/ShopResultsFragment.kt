6394b9cbde3e97c2686d2199690988ef849f1547
Vuong Xuan Hong <vuongxuanhong@gmail.com>
2018-06-04 14:59:23 +0700

    event listener

--- app/src/main/java/ishopgo/com/exhibition/ui/main/home/search/shop/ShopResultsFragment.kt
+++ app/src/main/java/ishopgo/com/exhibition/ui/main/home/search/shop/ShopResultsFragment.kt
@@ -1,6 +1,7 @@
 package ishopgo.com.exhibition.ui.main.home.search.shop
 
 import android.arch.lifecycle.Observer
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -8,12 +9,15 @@ import android.view.animation.AnimationUtils
 import ishopgo.com.exhibition.R
 import ishopgo.com.exhibition.domain.request.SearchProductRequest
 import ishopgo.com.exhibition.domain.request.SearchShopRequest
+import ishopgo.com.exhibition.domain.response.IdentityData
 import ishopgo.com.exhibition.domain.response.Shop
 import ishopgo.com.exhibition.model.Const
 import ishopgo.com.exhibition.ui.base.list.BaseListFragment
 import ishopgo.com.exhibition.ui.base.list.BaseListViewModel
+import ishopgo.com.exhibition.ui.base.list.ClickableAdapter
 import ishopgo.com.exhibition.ui.base.widget.BaseRecyclerViewAdapter
 import ishopgo.com.exhibition.ui.main.home.search.SearchViewModel
+import ishopgo.com.exhibition.ui.main.shop.ShopDetailActivity
 import kotlinx.android.synthetic.main.content_swipable_recyclerview.*
 
 /**
@@ -71,7 +75,21 @@ class ShopResultsFragment : BaseListFragment<List<SearchShopResultProvider>, Sea
     }
 
     override fun itemAdapter(): BaseRecyclerViewAdapter<SearchShopResultProvider> {
-        return SearchShopAdapter()
+        val searchShopAdapter = SearchShopAdapter()
+        searchShopAdapter.listener = object: ClickableAdapter.BaseAdapterAction<SearchShopResultProvider> {
+            override fun click(position: Int, data: SearchShopResultProvider, code: Int) {
+                context?.let {
+                    if (data is IdentityData) {
+                        val boothId = data.id
+                        val intent = Intent(context, ShopDetailActivity::class.java)
+                        intent.putExtra(Const.TransferKey.EXTRA_ID, boothId)
+                        startActivity(intent)
+                    }
+                }
+            }
+
+        }
+        return searchShopAdapter
     }
 
     override fun firstLoad() {
