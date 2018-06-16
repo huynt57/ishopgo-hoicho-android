4c1c56691c4c6893ac08abc22e790da52f78c102
Nguyễn Huy Hoàng <nguyenhuyhoang16@gmail.com>
2018-06-16 11:15:11 +0700

    add text hint vao editext tim kiem san pham lien quan

--- app/src/main/java/ishopgo/com/exhibition/ui/main/productmanager/detail/ProductManagerDetailFragment.kt
+++ app/src/main/java/ishopgo/com/exhibition/ui/main/productmanager/detail/ProductManagerDetailFragment.kt
@@ -399,11 +399,6 @@ class ProductManagerDetailFragment : BaseFragment() {
                     .canceledOnTouchOutside(false)
                     .build()
 
-            val edt_search_name = dialog.findViewById(R.id.edt_search_name) as TextInputEditText
-            val edt_search_code = dialog.findViewById(R.id.edt_search_code) as TextInputEditText
-            edt_search_name.hint = "Nhập tên sản phẩm"
-            edt_search_code.hint = "Nhập mã sản phẩm"
-
             dialog.show()
         }
     }
