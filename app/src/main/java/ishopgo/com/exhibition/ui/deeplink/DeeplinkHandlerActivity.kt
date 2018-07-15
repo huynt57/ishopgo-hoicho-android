80ee6a5e7df57ef32f31e397f0ed6f94021292ce
Vuong Xuan Hong <vuongxuanhong@gmail.com>
2018-07-15 07:37:12 +0700

    chinh lai deeplink handler

--- app/src/main/java/ishopgo/com/exhibition/ui/deeplink/DeeplinkHandlerActivity.kt
+++ app/src/main/java/ishopgo/com/exhibition/ui/deeplink/DeeplinkHandlerActivity.kt
@@ -2,6 +2,7 @@ package ishopgo.com.exhibition.ui.deeplink
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import com.google.firebase.dynamiclinks.FirebaseDynamicLinks
 import ishopgo.com.exhibition.model.Const
